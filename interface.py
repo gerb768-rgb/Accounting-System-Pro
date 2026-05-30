cb63a8c87061d5a764ff6b0479b9ffd36ce934a2
gerb768-rgb <gerb768@gmail.com>
2026-05-30 22:38:39 +0300

    Delete interface.py

--- interface.py
+++
@@ -1,6 +0,0 @@
-def show_menu(user_role):
-    print(f"\n--- قائمة النظام ({user_role}) ---")
-    if user_role == "مدير":
-        print("1. فحص المخزون | 2. التقارير | 3. حذف فاتورة")
-    else:
-        print("1. إدخال فاتورة | 2. فحص المخزون")
