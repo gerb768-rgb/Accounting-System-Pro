413c808b8b8f7641103be47965d4f2858a714ad5
gerb768-rgb <gerb768@gmail.com>
2026-05-30 22:06:15 +0300

    Update app_ui.py

--- app_ui.py
+++ app_ui.py
@@ -1,5 +1,15 @@
-# زر عرض الرسم البياني للمدير
-if user_role == "مدير":
-    buttons.append(ft.ElevatedButton("عرض رسوم المبيعات", on_click=lambda _: graphics.generate_sales_chart(data)))
-    buttons.append(ft.ElevatedButton("طباعة الفاتورة", on_click=lambda _: printer.print_invoice(invoice_id)))
-  
+import flet as ft
+import main     # المنطق الأساسي
+import graphics # كود الرسوم البيانية
+import printer  # كود الطباعة
+
+def main_app(page: ft.Page):
+    # هنا تضع كود الواجهة والداش بورد الذي قمنا بتصميمه سابقاً
+    # تأكد من استدعاء الوظائف من الملفات الأخرى عند ضغط الأزرار
+    # مثال: 
+    # ft.ElevatedButton("طباعة", on_click=lambda _: printer.print_invoice(id))
+    # ft.ElevatedButton("رسوم بيانية", on_click=lambda _: graphics.generate_sales_chart(data))
+    
+    page.update()
+
+ft.app(target=main_app)
