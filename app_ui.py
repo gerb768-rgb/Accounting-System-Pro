d1763c549f2f2ad7732ea01a18d4a7205fea8216
gerb768-rgb <gerb768@gmail.com>
2026-05-30 22:05:25 +0300

    Update app_ui.py

--- app_ui.py
+++ app_ui.py
@@ -1,2 +1,5 @@
-import graphics  # لاستخدام الرسوم البيانية
-import printer   # لاستخدام وظائف الطباعة
+# زر عرض الرسم البياني للمدير
+if user_role == "مدير":
+    buttons.append(ft.ElevatedButton("عرض رسوم المبيعات", on_click=lambda _: graphics.generate_sales_chart(data)))
+    buttons.append(ft.ElevatedButton("طباعة الفاتورة", on_click=lambda _: printer.print_invoice(invoice_id)))
+  
